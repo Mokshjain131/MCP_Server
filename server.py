ffc5d70b6c3997984fcaf484a47658df9d41a7e1
Mokshjain131 <mokshjain131@gmail.com>
2025-06-03 00:00:33 +0530

    basic layout

---
+++ server.py
@@ -0,0 +1,29 @@
+from mcp.server.fastmcp import FastMCP
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+# Create an MCP server
+mcp = FastMCP(
+    name="Veronica",
+    host="0.0.0.0",
+    port=8000,
+)
+
+# Add a tool
+@mcp.tool()
+def hello(name: str):
+    return f"Hello {name}"
+
+# Run the server
+if __name__ == "__main__":
+    transport = "stdio"
+    if transport == "stdio":
+        print("Running server with stdio transport")
+        mcp.run(transport="stdio")
+    elif transport == "sse":
+        print("Running server with SSE transport")
+        mcp.run(transport="sse")
+    else:
+        raise ValueError(f"Unknown transport: {transport}")
+
